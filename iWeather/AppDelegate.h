bfe31738ce3a92f3ed5af3659e48c7761df0e103
Mr.Bi <MrBi@Bis-MacBook-Pro.local>
2015-08-14 18:28:54 +0700

    Initial Commit

---
+++ iWeather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iWeather
+//
+//  Created by Mr.Bi on 8/14/15.
+//  Copyright (c) 2015 Mr.Bi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iWeather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iWeather
+//
+//  Created by Mr.Bi on 8/14/15.
+//  Copyright (c) 2015 Mr.Bi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
